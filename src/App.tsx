b2d8caa985cfd62405df35c3d1c5c939e6d3e770
Sabrie Sevil Hatipoglu <sevilhatipogluu93@gmail.com>
2021-07-17 00:31:09 +0100

    Fetch API

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react"
+
 
 function App() {
+
+// Fetching API
+  useEffect(() => {
+      const url = "https://run.mocky.io/v3/a811c0e9-adae-4554-9694-173aa23bc38b";
+
+      const fetchData = async () => {
+        try {
+          const response = await fetch(url);
+          const json = await response.json();
+          const data = json['media'].forEach((content:any) => {
+            console.log(content)
+          });
+          return data;
+        } catch (error) {
+          console.log("error", error);
+        }
+      };
+
+      fetchData();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Hello</h1>
     </div>
   );
 }

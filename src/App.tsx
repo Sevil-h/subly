09adb11912cb27cbac5c03e94da3e6a764150fed
Sabrie Sevil Hatipoglu <sevilhatipogluu93@gmail.com>
2021-07-18 23:24:41 +0100

    Added axios packages

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,19 @@
 import React from 'react';
-import { useEffect } from "react"
-
+import { useState, useEffect } from "react"
+import Cards from './components/Cards'
+import axios, {AxiosResponse} from 'axios';
 
 function App() {
+  const [contents, setContent] = useState([])
 
-// Fetching API
-  useEffect(() => {
-      const url = "https://run.mocky.io/v3/a811c0e9-adae-4554-9694-173aa23bc38b";
-
-      const fetchData = async () => {
-        try {
-          const response = await fetch(url);
-          const json = await response.json();
-          const data = json['media'].forEach((content:any) => {
-            console.log(content)
-          });
-          return data;
-        } catch (error) {
-          console.log("error", error);
-        }
-      };
-
-      fetchData();
-  }, []);
 
   return (
     <div className="container">
       <h1>Hello</h1>
+      <Cards />
+      <>
+
+      </>
     </div>
   );
 }
---
+++ src/components/Cards.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { useState } from 'react';
+
+const Cards = () => {
+  const [cards, setCards] = useState([])
+
+  return (
+    <>
+  </>
+  )
+}
+
+export default Cards
